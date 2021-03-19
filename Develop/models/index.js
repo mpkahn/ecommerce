c39a2a61a1286f40ac95959b1dc0ba62df69b662
Michael Kahn <kahn.mp@gmail.com>
2021-03-18 17:23:06 -0700

    moving files from HW folder

---
+++ Develop/models/index.js
@@ -0,0 +1,33 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "caregory_id"
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "caregoryID"
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id"
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
